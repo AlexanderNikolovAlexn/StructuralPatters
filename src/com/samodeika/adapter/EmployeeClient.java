f023705f9a4ea75ce57b7f3ccf85de60d0e9edb4
Alexander Nikolov <alexander.nikolov.alexn@gmail.com>
2015-10-05 14:41:20 +0300

    added Ldap,CSV adapters for imaginary employee class

--- src/com/samodeika/adapter/EmployeeClient.java
+++ src/com/samodeika/adapter/EmployeeClient.java
@@ -12,9 +12,11 @@ public class EmployeeClient {
 		Employee employeeFromDB = new EmployeeDB("1234", "John", "Wick", "john.wick@gmail.com");		
 		employees.add(employeeFromDB);
 		
-		EmployeeLdap employeeLdap = new EmployeeLdap("chewie", "Solo", "Han", "solo.han@gmail.com");
+		EmployeeLdap employeeLdap = new EmployeeLdap("chewie", "Solo", "Han", "solo.han@gmail.com");		
+		employees.add(new EmployeeAdapterLdap(employeeLdap));
 		
-		employees.add(new EmployeeAdapterLdap(employeeLdap));		
+		EmployeeCSV employeeCsv = new EmployeeCSV("567,Sherlock,Holmes,scherlock.holmes@gmail.com");		
+		employees.add(new EmployeeAdapterCSV(employeeCsv));
 		
 		return employees;
 		
